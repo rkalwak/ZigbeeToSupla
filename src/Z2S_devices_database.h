2382f8ccb3fcea1b6f90c1997c0c544aad47f069
Rafał Kałwak <Rafa Kawak>
2025-01-18 19:51:19 +0100

    switch working

--- src/Z2S_devices_database.h
+++ src/Z2S_devices_database.h
@@ -122,7 +122,7 @@ static z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
     .z2s_device_desc_id = Z2S_DEVICE_DESC_ON_OFF, .z2s_device_endpoints_count = 1},
 
   {.manufacturer_name = "_TZ3000_gjrubzje", .model_name = "TS0001", 
-    .z2s_device_desc_id = Z2S_DEVICE_DESC_ON_OFF, .z2s_device_endpoints_count = 1},
+    .z2s_device_desc_id = Z2S_DEVICE_DESC_RELAY_ELECTRICITY_METER, .z2s_device_endpoints_count = 1},
 };
 
 //_TZ3000_wkai4ga5
