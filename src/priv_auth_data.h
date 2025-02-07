dde567e0223ec9bb8bdc01255a74fe8b620e3890
Rafał Kałwak <Rafa Kawak>
2025-02-07 06:48:45 +0100

    fix

--- src/priv_auth_data.h
+++ src/priv_auth_data.h
@@ -1,9 +1,9 @@
-char SUPLA_WIFI_SSID[20]="";
-char SUPLA_WIFI_PASS[50]="";
-char SUPLA_EMAIL[50]="";
-char SUPLA_SVR[50]="";
+static char SUPLA_WIFI_SSID[20]="";
+static char SUPLA_WIFI_PASS[50]="";
+static char SUPLA_EMAIL[50]="";
+static char SUPLA_SVR[50]="";
 
-char GUID[SUPLA_GUID_SIZE] = {0xA4};
-char AUTHKEY[SUPLA_AUTHKEY_SIZE] = {0x09};
-int ZG_OPEN_NETWORK=20;
-
+static char GUID[SUPLA_GUID_SIZE] = {0xA4};
+static char AUTHKEY[SUPLA_AUTHKEY_SIZE] = {0x09};
+static int ZG_OPEN_NETWORK=20;
+static int ZG_SCENE_SWITCH_DEBOUNCE_TIME_MS=1500;
